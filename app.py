9811645cd31b66ae6abe12825bb1109543fbebae
contengee <contengee@gmail.com>
2023-05-20 02:46:31 +0900

    DATABASE_URL bug fixed3

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-# Herokuにデプロイされた場合は、Herokuが提供する環境変数からデータベースの接続情報を取得します
-if 'DATABASE_URL' in os.environ:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-else:
-    # Heroku以外の環境
+if os.environ.get('DATABASE_URL') is None: # ローカル
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlite:////tmp/test.db'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] # Heroku
+    
 db = SQLAlchemy(app)
 
 class Word(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(80), unique=True, nullable=False)
-    
-    def __init__(self, word):
-        self.word = word
-    
+
+#    def __init__(self, word):
+#        self.word = word
+
     def __repr__(self):
         return '<Word %r>' % self.word
 
