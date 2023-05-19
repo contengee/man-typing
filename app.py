5c12d472915e2d5cf2fdd0bcc724705d08dd9852
contengee <contengee@gmail.com>
2023-05-20 04:58:02 +0900

    heroku_runback2

--- app.py
+++ app.py
@@ -37,6 +37,5 @@ class Word(db.Model):
         return jsonify(word=word.word)
 
     if __name__ == "__main__":
-
+        app.run(debug=True)
     db.create_all()  # データベースとテーブルを作成します。
-    app.run(debug=True)
