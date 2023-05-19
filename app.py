43658cb09aa38b58c9209428f3f4bade67eee757
contengee <contengee@gmail.com>
2023-05-20 03:00:26 +0900

    DATABASE_URL bug fixed4

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-if os.environ.get('DATABASE_URL') is None: # ローカル
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlite:////tmp/test.db'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] # Heroku
+#if os.environ.get('DATABASE_URL') is None: # ローカル
+#    SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlite:////tmp/test.db'
+#    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#else:
+#    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] # Heroku
+
+SQLALCHEMY_DATABASE_URI = 'postgres://ddsllujzsltuhx:db525577117eb2c7661c4e13e7bb9894039eff2a80699fec05bc65dd8a706005@ec2-34-193-110-25.compute-1.amazonaws.com:5432/d7696h6kmr48s0'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] # Heroku
     
 db = SQLAlchemy(app)
 
