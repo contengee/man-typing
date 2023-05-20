a494ee8a9776d0aaf7192ecafeff4cd8ec9818cc
contengee <contengee@gmail.com>
2023-05-21 01:56:24 +0900

    APT_REPO2

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 #    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 #    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
+# temp
 DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_YELLOW_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 
